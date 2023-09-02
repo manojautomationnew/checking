74e9aa99e39dc1c80e7c1cc989a52839b9c6d3b6
manojautomationnew <gituse5@gmail.com>
2023-09-03 01:35:06 +0530

    here checking the files

---
+++ Gitwork.java
@@ -0,0 +1,12 @@
+package learning;
+
+public class Gitwork {
+
+	public static void main(String[] args) {
+
+//		my name is manoj here i am practicing the lines to learn from the range of situation
+		
+		
+	}
+
+}
