63933c537fac8b27ad60f8b3d1582f11d22569a7
manojautomationnew <gituse5@gmail.com>
2023-09-03 01:59:55 +0530

    modified the 2nd java file

--- Gitwork2.java
+++ Gitwork2.java
@@ -1,5 +1,10 @@
-public class test{
-public static void main (string[] args){
+package learning;
+
+public class Gitwork2 {
+
+	public static void main(String[] args) {
 // nothing here to show some of the files to check here and go
-}
+
+	}
+
 }
