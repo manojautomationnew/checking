1c77c9fb50f8a6dda04b1071ebd2956443904ce3
manojautomationnew <gituse5@gmail.com>
2023-09-03 09:24:06 +0530

    may be conflict occur

--- Gitwork2.java
+++ Gitwork2.java
@@ -6,6 +6,8 @@ public class Gitwork2 {
 // nothing here to show some of the files to check here and go
 		
 //		there is another file need to update everything to understand
+		
+		// there is a situation that we can write or do anything that has come to the monster
 
 	}
 
